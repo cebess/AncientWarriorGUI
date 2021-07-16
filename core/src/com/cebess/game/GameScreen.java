1a67d2b4a612634167d1908828ad21b27f174d86
Charlie Bess <cebess@users.noreply.github.com>
2021-07-16 05:22:24 -0400

    Initial commit

---
+++ core/src/com/cebess/game/GameScreen.java
@@ -0,0 +1,47 @@
+package com.cebess.game;
+
+import com.badlogic.gdx.Screen;
+
+class GameScreen implements Screen {
+
+    //screen
+
+    //graphics
+
+    //timing
+    
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ desktop/src/com/cebess/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cebess.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cebess.game.AncientWarrior;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AncientWarrior(), config);
+	}
+}
